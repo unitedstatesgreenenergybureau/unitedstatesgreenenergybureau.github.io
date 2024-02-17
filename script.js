1399c28811e349b47d2da5949b409181c8cfe9d2
heelsboroharry <74061248+heelsboroharry@users.noreply.github.com>
2024-02-16 20:51:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+
+
 function toggleModal() {
-  const modal = document.getElementById('modal-window');
+  const modalchild = document.getElementById('modal-window');
+  const modal = modalchild.parentNode;
   modal.classList.toggle('hidden');
   document.body.classList.toggle('modal-open'); // add/remove class for darkening
+  console.log("modal triggered");
 }
 const link = document.getElementById('popuplink'); // replace with your link ID
 link.addEventListener('click', toggleModal);
